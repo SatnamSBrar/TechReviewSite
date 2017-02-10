47596ae4d21798cf80dcd0c456df4d61c39e7849
Satnam Brar <satnamsbrar@gmail.com>
2017-02-10 13:39:10 -0500

    completed Review and Category Models

---
+++ TechReviewMVC/Models/Category.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace TechReviewMVC.Models
+{
+    public class Category
+    {
+        [Key]
+        public int CategoryID { get; set; }
+
+        [Display(Name = "Category")]
+        public string CategoryName { get; set; }
+
+        public virtual ICollection<Review> Reviews { get; set; }
+    }
+}
---
+++ TechReviewMVC/Models/Review.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace TechReviewMVC.Models
+{
+    public class Review
+    {
+        [Key]
+        public int ReviewID { get; set; }
+
+        [ForeignKey("Category")]
+        public int CategoryID { get; set; }
+        public virtual Category Category { get; set; }
+
+
+        public string ReviewTitle { get; set; }
+        public string ReviewBody { get; set; }
+        public DateTime Date { get; set; }
+        public int Starred { get; set; }
+    }
+}
